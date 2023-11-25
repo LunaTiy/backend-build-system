332f4cd3a9030d67ce74047069584b7b1415dc7f
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-11-25 15:20:15 +0300

    feat: implement builds reader functional, convert yaml files to utf-8 encoding

---
+++ app/build_system/builds_reader.py
@@ -0,0 +1,40 @@
+﻿import yaml
+
+
+class BuildsReader:
+    @staticmethod
+    def read_yaml_file(path: str) -> dict:
+        with open(path, 'r') as stream:
+            try:
+                parsed_yaml: dict = yaml.safe_load(stream)
+            except yaml.YAMLError as ex:
+                print(ex)
+
+        return parsed_yaml
+
+    def read_builds(self, path: str) -> dict[str, list[str]]:
+        parsed_yaml = self.read_yaml_file(path)
+
+        builds = parsed_yaml['builds']
+        builds_graph: dict[str, list[str]] = {}
+
+        for build in builds:
+            name, tasks = build['name'], build['tasks']
+            builds_graph[name] = tasks
+
+        return builds_graph
+
+    def read_tasks(self, path: str) -> dict[str, list[str]]:
+        parser_yaml = self.read_yaml_file(path)
+
+        tasks = parser_yaml['tasks']
+        tasks_graph: dict[str, list[str]] = {}
+
+        for task in tasks:
+            name, dependencies = task['name'], task['dependencies']
+            tasks_graph[name] = dependencies
+
+        return tasks_graph
+
+
+build_reader = BuildsReader()
