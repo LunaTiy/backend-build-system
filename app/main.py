4d74c82b318f3962bc959ea9053f2e32a5b0756e
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-11-25 18:25:02 +0300

    refactor: save paths to build files into constants

--- app/main.py
+++ app/main.py
@@ -4,11 +4,14 @@ from fastapi import FastAPI
 
 from app.build_system.builds import TasksBuilder
 from app.build_system.router import router as build_system_router
+from app.constants import PATH_TO_TASKS, PATH_TO_BUILDS
 
 
 @asynccontextmanager
-async def lifespan(fastapi_app: FastAPI) -> AbstractAsyncContextManager[None]:
-    TasksBuilder.init_tasks_and_build("builds/tasks.yaml", "builds/builds.yaml")
+async def lifespan(
+        fastapi_app: FastAPI  # noqa
+) -> AbstractAsyncContextManager[None]:
+    TasksBuilder.init_tasks_and_build(PATH_TO_TASKS, PATH_TO_BUILDS)
     yield
 
 
