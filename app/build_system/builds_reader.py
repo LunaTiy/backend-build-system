5388ab8279ce1983aca9caad43b3735780222d48
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-11-25 15:37:44 +0300

    feat: add project exceptions, handle BuildsReader, inject logger

--- app/build_system/builds_reader.py
+++ app/build_system/builds_reader.py
@@ -1,38 +1,55 @@
-﻿import yaml
+﻿import os.path
+
+import yaml
+
+from app.exceptions import IncorrectBuildFileException, BuildFileNotExists
+from app.logger import logger
 
 
 class BuildsReader:
     @staticmethod
     def read_yaml_file(path: str) -> dict:
+        if not os.path.isfile(path):
+            raise BuildFileNotExists(path)
+
         with open(path, 'r') as stream:
             try:
                 parsed_yaml: dict = yaml.safe_load(stream)
             except yaml.YAMLError as ex:
-                print(ex)
+                logger.exception(ex)
+                raise IncorrectBuildFileException(path)
 
         return parsed_yaml
 
     def read_builds(self, path: str) -> dict[str, list[str]]:
         parsed_yaml = self.read_yaml_file(path)
 
-        builds = parsed_yaml['builds']
-        builds_graph: dict[str, list[str]] = {}
+        try:
+            builds = parsed_yaml['builds']
+            builds_graph: dict[str, list[str]] = {}
 
-        for build in builds:
-            name, tasks = build['name'], build['tasks']
-            builds_graph[name] = tasks
+            for build in builds:
+                name, tasks = build['name'], build['tasks']
+                builds_graph[name] = tasks
+        except KeyError as ex:
+            logger.exception(ex)
+            raise IncorrectBuildFileException(path)
 
         return builds_graph
 
     def read_tasks(self, path: str) -> dict[str, list[str]]:
         parser_yaml = self.read_yaml_file(path)
 
-        tasks = parser_yaml['tasks']
-        tasks_graph: dict[str, list[str]] = {}
+        try:
+            tasks = parser_yaml['tasks']
+            tasks_graph: dict[str, list[str]] = {}
 
-        for task in tasks:
-            name, dependencies = task['name'], task['dependencies']
-            tasks_graph[name] = dependencies
+            for task in tasks:
+                name, dependencies = task['name'], task['dependencies']
+                tasks_graph[name] = dependencies
+        except KeyError as ex:
+            logger.exception(ex)
+            raise IncorrectBuildFileException(path)
 
         return tasks_graph
 
---
+++ app/exceptions.py
@@ -0,0 +1,37 @@
+﻿from fastapi import HTTPException, status
+
+
+class BuildsSystemException(HTTPException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail = ""
+
+    def __init__(
+            self,
+            status_code: status = status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail: str = ""
+    ) -> None:
+        self.status_code = status_code
+        self.detail = detail
+        super().__init__(status_code=self.status_code, detail=self.detail)
+
+
+class BuildFileNotExists(BuildsSystemException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail = "Build file not exists: "
+
+    def __init__(
+            self,
+            file_path: str
+    ) -> None:
+        super().__init__(self.status_code, self.detail + file_path)
+
+
+class IncorrectBuildFileException(BuildsSystemException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail = "Build is incorrect: "
+
+    def __init__(
+            self,
+            file_path: str
+    ) -> None:
+        super().__init__(self.status_code, self.detail + file_path)
